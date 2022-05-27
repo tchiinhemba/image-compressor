3a32df11c9113b494130b506216841b8c87465b0
tchiinhemba <tchiinhemba@outlook.com>
2022-05-27 15:41:30 +0100

    update

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+const compress_images = require("compress-images");
+
+function start() {
+  compress_images(
+    "input/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}",
+    "output/",
+    { compress_force: false, statistic: true, autoupdate: true },
+    false,
+    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+    { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+    { svg: { engine: "svgo", command: "--multipass" } },
+    {
+      gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] },
+    },
+    function (err, completed) {
+      if (completed === true) {
+        console.log("Your compression finished!")
+      }
+    }
+  );
+}
+
+
+
+start();
